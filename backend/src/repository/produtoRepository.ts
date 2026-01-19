ad0fbacc9d980b54f08546f24c318dbc78540e91
JG1511 <joaotsi2024@gmail.com>
2026-01-19 11:19:18 -0300

    repository e services de produto, por enquanto finalizados

---
+++ backend/src/repository/produtoRepository.ts
@@ -0,0 +1,62 @@
+import { prisma } from "../db/db";
+
+class ProdutoRepository {
+    async findAll() {
+        const result = await prisma.produto.findMany();
+        return result;
+    }
+
+    async findId(id: string) {
+        const result = await prisma.produto.findUnique({
+            where: { id_produto: id }
+        })
+
+        return result;
+    }
+
+    async create(nome: string, descricao: string, preco: number, cor: string, modelo: string, categoriaId: number) {
+        const result = await prisma.produto.create({
+            data: {
+                nome: nome,
+                descricao: descricao,
+                preco: preco,
+                cor: cor,
+                modelo: modelo,
+                categoria: {
+                    connect: { id_categoria: categoriaId }
+                }
+            }
+        });
+
+        return result;
+    }
+
+    async update(id: string, nome: string, descricao: string, preco: number, cor: string, modelo: string, categoriaId: number) {
+        const result = await prisma.produto.update({
+            where: {
+                id_produto: id
+            },
+            data: {
+                nome: nome,
+                descricao: descricao,
+                preco: preco,
+                cor: cor,
+                modelo: modelo,
+                categoria: {
+                    connect: { id_categoria: categoriaId }
+                }
+            }
+        });
+
+        return result
+    }
+
+    async delete(id: string) {
+        await prisma.produto.delete({
+            where: { id_produto: id }
+        })
+    }
+}
+
+export default new ProdutoRepository();
+
--- backend/src/services/produtoServicer.ts
+++ backend/src/services/produtoServicer.ts
@@ -1,5 +1,41 @@
+import { prisma } from "../db/db";
+import ProdutoRepository from "../repository/produtoRepository"
+
 class ProdutoServicer {
-    
+
+    async getProduto() {
+        const rows = ProdutoRepository.findAll();
+        return rows;
+    }
+
+    async getProdutoId(id: string) {
+        const row = ProdutoRepository.findId(id);
+        return row;
+    }
+
+    async createProduto(nome: string, descricao: string, preco: number, cor: string, modelo: string, categoriaId: number) {
+        const row = ProdutoRepository.create(nome, descricao, preco, cor, modelo, categoriaId);
+        return row;
+    }
+
+    async updateProduto(id: string, nome: string, descricao: string, preco: number, cor: string, modelo: string, categoriaId: number) {
+        const produtoExist = await prisma.produto.findUnique({
+            where: { id_produto: id }
+        });
+
+        if (!produtoExist) {
+            throw new Error('Produto não existente')
+        } else {
+            const row = ProdutoRepository.update(id, nome, descricao, preco, cor, modelo, categoriaId);
+            return row;
+        }
+
+    }
+
+    async deleteProduto(id: string) {
+        ProdutoRepository.delete(id);
+    }
+
 }
 
 export default new ProdutoServicer()
