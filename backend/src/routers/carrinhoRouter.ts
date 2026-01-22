6829cd747c1abb463f13a3de47daae2aba61fdc8
JG1511 <joaotsi2024@gmail.com>
2026-01-22 14:35:06 -0300

    resolvendo o bug

--- backend/src/routers/carrinhoRouter.ts
+++ backend/src/routers/carrinhoRouter.ts
@@ -3,7 +3,7 @@ import CarrinhoController from "../controllers/carrinhoController";
 
 const carrinhoRouter = Router();
 
-carrinhoRouter.get('/listar_carrinho:/id', CarrinhoController.getCarrinho);
+carrinhoRouter.get('/listar_carrinho/:id', CarrinhoController.getCarrinho);
 carrinhoRouter.get('/listar_produto_no_carrinho/:id', CarrinhoController.listProdutoNoCarrinho);
 carrinhoRouter.post('/criar_carrinho/:id', CarrinhoController.createCarrinho);
 carrinhoRouter.post('/add_produto_no_carrinho/:id', CarrinhoController.addProdutoNoCarrinho);
